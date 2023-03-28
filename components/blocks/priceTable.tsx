0a2879a7af4e9dbd8c8a2ca8bf99d7062f1566f8
Mor-ald <mor.ald@yandex.ru>
2023-03-28 13:56:46 +0300

    fix import

--- components/blocks/priceTable.tsx
+++ components/blocks/priceTable.tsx
@@ -1,6 +1,6 @@
 import { Section } from "../util/section";
 import type { TinaTemplate } from "tinacms";
-import { TreeTable } from 'primereact/treeTable';
+import { TreeTable } from 'primereact/treetable';
 import { Column } from 'primereact/column';
 import { useEffect, useState } from "react";
 import axios from "axios";
