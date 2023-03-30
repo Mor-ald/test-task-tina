be7433178e33ae6df40f85ba885b2c5ef866c06b
Alexey <Mor.ald@yandex.ru>
2023-03-30 14:06:24 +0300

    fix app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/prop-types */
+
 import "../styles.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 import "primereact/resources/primereact.css";
+
+import React from "react";
 
 const App = ({ Component, pageProps }) => {
 	return <Component {...pageProps} />;
